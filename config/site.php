3aa2ad4b5dd786e84c6e5e5ebd6c698f8c9483d2
moison thomas <moison.thomas@geii-209-06.iutcb.univ-littoral.fr>
2013-01-09 16:36:01 +0100

    installation

---
+++ config/site.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'test');
+define('PASSWORD_SALT', 'sI0Iw4attOOR2n7jmEHqUJiXvvzpcZj6pzjkwMvyxRcw5iGeb4VVo1WB87usUo68');
