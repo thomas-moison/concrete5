23462584d18db804177d7c103829a6c22fe66c92
moison.thomas <T.Moison@mel-etu.univ-littoral.fr>
2013-01-18 13:08:18 +0100

    copie du code de default.php dans acceuil.php

--- themes/bootstrap/accueil.php
+++ themes/bootstrap/accueil.php
@@ -1,6 +1,35 @@
 <?php $this->inc('elements/header.php');?>
-
-//contenu
-
-
+<div class="row">
+		    <div class="span10">
+		        <div class="hero-unit">
+				<?php
+				$a= new Area('Contenu');
+				$a->display($c);
+				?>
+				  <h1>Bootstrap starter template</h1>
+				  <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+				  
+				  <a class="btn btn-primary btn-large">Plus d'infos</a>
+				</div>
+		      
+		          	
+		      	
+		      </div>
+		      
+		    </div>
+		    <div class="span2">
+		    	<div class="well well-small">
+				<?php
+				$a= new Area('lateral');
+				$a->display($c);
+				?>
+					<h3>Contenu latéral</h3>
+					<p>
+						Curabitur non felis in risus eleifend ultricies. Sed a tellus eros, a euismod ipsum.
+					</p>
+				</div>
+		    </div>
+	    </div>
+	    
+	  
 <?php $this->inc('elements/footer.php');?>
